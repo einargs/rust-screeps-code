363c551779f8312d16f0784f323b0f04034422a0
Einar Strandberg <einargs@gmail.com>
2024-01-02 18:46:02 -0600

    well working segmentation

--- src/rooms/room_cut.rs
+++ src/rooms/room_cut.rs
@@ -47,13 +47,6 @@ const BORDER_COLOR: Color = 1;
 /// The first valid color.
 const START_COLOR: Color = 2;
 
-/// Check if two colors are different values, and that neither
-/// is NO_COLOR.
-#[inline]
-fn diff_color(a: Color, b: Color) -> bool {
-  a != 0 && b != 0 && a != b
-}
-
 struct Maxima {
   xy: RoomXY,
   // TODO: may remove this field
@@ -74,58 +67,14 @@ fn taxicab_adjacent(xy: RoomXY) -> impl Iterator<Item = RoomXY> {
     .filter_map(move |dir| xy.checked_add_direction(dir))
 }
 
-// TODO: we're going to use a custom disjoint set and then build a custom
-// color map for better display with smaller unique indices. I can do that
-// with a BTreeMap.
-/// Data structure used for assigning small unique colors to stuff
-/// after building a disjoint set data structure from tiles.
-struct ColorMapI {
-  color_count: u16,
-  map: BTreeMap<usize, u16>
-}
-
-/// Find the local maxima.
-///
-/// If we have two points of the same height surrounded by lesser points,
-/// we count them both as maxima. We will perform a merge operation.
-fn find_maxima<'a>(dist: &'a DistMatrix) -> impl Iterator<Item = Maxima> + 'a {
-  all_room_xy().filter_map(move |xy| {
-    let height = dist.get(xy);
-    for adj in surrounding_xy(xy) {
-      if dist.get(adj) >= height {
-        return None
-      }
-    }
-    Some(Maxima { xy, height })
-  })
-}
-
 type ColorMap = TileMap<Color>;
-
 
 // Here's my idea: what if we do two passes using dts or something
 // similar.
 // one to identify the maxima, and then we do another flooding
 // pass to identify the borders.
 
-// TODO: get a name that implies it isn't involved in actually
-// coloring.
-
-// NOTE: below is old
-// we check the height of nearby tiles, looking for
-// ones higher than us.
-//
-// if it's only one, we union join with it.
-//
-// If there are two or more higher than us, we recurse
-// and then check if they're all in the same set.
-// - If they are all in the same set, we join them.
-// - Otherwise, we add ourselves to the border list.
-//
-// It is possible a border tile to end up in a set. Imagine
-// a tile whose only neighbor is a tile on the border of
-// two rooms.
-
+// TODO: document logic/process
 /// Visit a tile to add it to the DisjointTileSet regions.
 ///
 /// Returns true if we found a higher tile.
@@ -151,16 +100,6 @@ fn link_to_maxima<'a>(
   let get_neighbors = surrounding_xy;
   // let get_neighbors = taxicab_adjacent;
 
-
-  // TODO: I have a correct maxima but the
-
-
-  // the problem is that there's no back tracking to ensure we don't end
-  // up in a dead end.
-  //
-  // Solutions:
-  // - make other things connect to us (bad track record)
-  // - breadth first search?
   if let Some(higher) = get_neighbors(xy)
     .find(|adj| height_map.get(*adj) > height) {
       let was_single = dts.is_singleton_xy(higher);
@@ -191,120 +130,20 @@ fn link_to_maxima<'a>(
   return false
 }
 
-/// Visit a tile to add it to the DisjointTileSet regions.
-fn color_tile<'a>(
-  xy: RoomXY,
-  height_map: &'a DistMatrix,
-  dts: &mut DisjointTileSet<'a>
-) {
-  let height = height_map.get(xy);
-
-  let cur_maxima_height = dts.maxima_height_for_xy(xy);
-
-  // not sure if we want this or taxicab.
-  let get_neighbors = surrounding_xy;
-
-  for adj in get_neighbors(xy) {
-    if dts.maxima_height_for_xy(adj) > cur_maxima_height {
-      continue
-    }
-    dts.union_xy(adj, xy);
-  }
-
-  /*
-  for adj in get_neighbors(xy).filter(|adj| height_map.get(*adj) == height) {
-    dts.union_xy(xy, adj);
-  }
-  */
-
-  /*
-  let higher_neighbors: Vec<RoomXY> = surrounding_xy(xy)
-    .filter(|adj| height_map.get(*adj) > height)
-    .collect();
-
-  match higher_neighbors.as_slice() {
-    [] => (),
-    /* should be equivalent to clause below
-    [adj] => {
-      let adj = *adj;
-      color_tile(adj, visited, border_tiles, height_map, dts);
-      dts.union_xy(xy, adj);
-      return
-    } */
-    all@[first, rest@..] => {
-      let first = *first;
-      for &adj in all {
-        color_tile(adj, visited, border_tiles, height_map, dts);
-      }
-      let id = dts.find_xy(first);
-      if rest.iter().all(|adj| dts.find_xy(*adj) == id) {
-        dts.union_xy(xy, first)
-      } else {
-        border_tiles.push(xy);
-      }
-      return
-    }
-  }
-
-  let other_neighbors = || surrounding_xy(xy)
-    .filter(|adj| height_map.get(*adj) <= height);
-  // temporary
-  for &adj in other_neighbors() {
-    color_tile(adj, visited, border_tiles, height_map, dts);
-  }
-  let mut neighbors = other_neighbors();
-  if let Some(first) = neighbors.next() {
-    let set = dts.find_xy(first);
-    if neighbors.all(|adj| dts.find_xy(*adj) == set) {
-      dts.
-    }
-  }
-  */
-}
-
-/// Creates the color map and the maximas.
-fn create_color_map(
-  height_map: &DistMatrix
-) -> (HashSet<RoomXY>, Box<ColorMap>, Color) {
-  let mut dts = DisjointTileSet::new_box(height_map);
-  let mut visited = TileMap::<bool>::new_box(false);
-
-  for (xy, idx) in all_room_xy_and_idx() {
-    // skip if it's a wall
-    if height_map.get_index(idx as usize) == 0 {
-      continue
-    }
-    // skip if we've *definitely* visited already
-    if !dts.is_singleton(idx as u16) {
-      continue
-    }
-    link_to_maxima(xy, height_map, &mut dts, false);
-  }
-  /*
-  for maxima in maximas {
-    let adj_color_xy = surrounding_xy(maxima.xy)
-      .find(|xy| color_map[*xy] != 0);
-    match adj_color_xy {
-      Some(adj_xy) => {
-        color_map[maxima.xy] = color_map[adj_xy];
-      }
-      None => {
-        color_map[maxima.xy] = color_count;
-        color_count += 1;
-      }
-    }
-  }
-  */
+/// Returns a color map for the disjoint tile set and
+/// a color that is the highest color value used + 1.
+fn color_disjoint_tile_set(
+  height_map: &DistMatrix,
+  dts: &mut DisjointTileSet,
+) -> (Box<ColorMap>, Color) {
   let mut color_map = ColorMap::new_box(0);
   let mut color_count: Color = START_COLOR;
   // tracks colors assigned to already encountered sets
   let mut color_index: BTreeMap<u16, Color> = BTreeMap::new();
 
+  // color the map and create a list of indexes.
   for idx in 0..(ROOM_AREA as u16) {
     if height_map.get_index(idx as usize) == 0 {
-      continue
-    }
-    if color_map[idx as usize] != 0 {
       continue
     }
     let set = dts.find(idx);
@@ -317,13 +156,50 @@ fn create_color_map(
   }
   debug!("Created {} colors!", color_count - 1);
 
-  let maximas: HashSet<RoomXY> = all_room_xy()
-    .filter(|xy| height_map.get(*xy) != 0)
-    .map(|xy| dts.maxima_for_xy(xy))
-    .collect();
-  debug!("maxima #{}", maximas.len());
+  (color_map, color_count)
+}
 
-  (maximas, color_map, color_count)
+/// Get a list of maximas.
+///
+/// Creates a list of local maxima and a [`DisjointTileSet`]
+/// of how it was found. This is not good segmentation, but
+/// is useful for debugging.
+fn calculate_local_maxima(
+  height_map: &DistMatrix
+) -> (impl Iterator<Item = RoomXY>, Box<DisjointTileSet>) {
+  let mut dts = DisjointTileSet::new_box(height_map);
+
+  for (xy, idx) in all_room_xy_and_idx() {
+    // skip if it's a wall
+    if height_map.get_index(idx as usize) == 0 {
+      continue
+    }
+    // skip if we've *definitely* visited already
+    if !dts.is_singleton(idx as u16) {
+      continue
+    }
+    link_to_maxima(xy, height_map, &mut dts, false);
+  }
+
+  let mut maxima_map: BTreeMap<u16, RoomXY> = BTreeMap::new();
+
+  // color the map and create a list of indexes.
+  for idx in 0..(ROOM_AREA as u16) {
+    if height_map.get_index(idx as usize) == 0 {
+      continue
+    }
+    let maxima = dts.maxima_for(idx);
+    maxima_map.entry(idx).or_insert(maxima);
+  }
+
+  (maxima_map.into_values(), dts)
+}
+
+/// Check if two colors are different values, and that neither
+/// is NO_COLOR.
+#[inline]
+fn diff_color(a: Color, b: Color) -> bool {
+  a != NO_COLOR && b != NO_COLOR && a != b
 }
 
 /// Flood a color map, avoiding coloring a cell when it would cause
@@ -331,48 +207,63 @@ fn create_color_map(
 /// Returns a list of coordinates that are on the border between
 /// two colors.
 ///
-/// Uses BFS (which should be fair?).
-///
-/// NOTE: this part of the algorithm could go wrong pretty easily.
+/// Uses BFS with a priority queue, which maintains fairness.
 fn flood_color_map(
   height_map: &DistMatrix,
-  color_map: &mut ColorMap,
-  maximas: &[Maxima]
-) -> Vec<RoomXY> {
-  let mut queue: VecDeque<RoomXY> = maximas
-    .iter().map(|m| m.xy).collect();
-  let mut visited = TileMap::<bool>::new_box(false);
+  maximas: impl Iterator<Item = RoomXY>
+) -> (Box<ColorMap>, Color, Vec<RoomXY>) {
+  use priority_queue::PriorityQueue;
+  let mut queue: PriorityQueue<RoomXY, Height> = PriorityQueue::new();
   let mut borders: Vec<RoomXY> = Vec::new();
+  let mut color_map = ColorMap::new_box(NO_COLOR);
 
-  while let Some(xy) = queue.pop_front() {
-    for adj in surrounding_xy(xy) {
-      if visited[adj] {
+  let mut color_count = START_COLOR;
+  for maxima in maximas {
+    color_map[maxima] = color_count;
+    color_count += 1;
+    let height = height_map.get(maxima);
+    queue.push(maxima, height);
+  }
+
+  while let Some((xy, height)) = queue.pop() {
+    let xy_color = color_map[xy];
+    // we should have a color assigned.
+    assert!(xy_color > 1, "color at {xy} did not exist; instead {xy_color}");
+    // we use taxicab because that's the distance metric
+    // used, and otherwise it could mess up the queue
+    // by jumping down 2 levels instead of 1.
+    // well, the priority queue would probably keep it safe, but
+    // why bother. This should keep the size of the queue smaller
+    // over time at least.
+    for adj in taxicab_adjacent(xy) {
+      /*
+      match color_map[adj] {
+        BORDER_COLOR => {
+          continue
+        }
+        other_color => {
+          if other_color != xy_color {
+            color_map[xy] = BORDER_COLOR;
+          }
+          continue
+        }
+        _ => (),
+      }
+      */
+      if color_map[adj] != NO_COLOR {
         continue
       }
-      visited[adj] = true;
       if height_map.get(adj) == 0 {
         continue
       }
-
-      if color_map[adj] != 0 {
-        continue
-      }
+      let adj_height = height_map.get(adj);
 
       color_map[adj] = color_map[xy];
-      queue.push_back(adj);
-      /*
-      if diff_color(color_map[xy], color_map[adj]) {
-        // we now need to change this color back.
-        color_map[xy] = 1;
-        borders.push(xy);
-      } else {
-        color_map[adj] = color_map[xy];
-        queue.push_back(adj);
-      }*/
+      queue.push(adj, adj_height);
     }
   }
 
-  borders
+  (color_map, color_count, borders)
 }
 
 pub enum Render {
@@ -394,7 +285,7 @@ fn calculate_color(total_count: Color, color: Color) -> String {
   // that into the U/V plane with fixed brightness.
   let u: f64 = radians.cos();
   let v: f64 = radians.sin();
-  let y: f64 = 0.5; // brightness.
+  let y: f64 = 1.0; // brightness.
 
   // now we convert into RGB.
   let red: f64 = y + v/0.88;
@@ -402,44 +293,57 @@ fn calculate_color(total_count: Color, color: Color) -> String {
   let blue: f64 = y + u/0.49;
   // debug!("Radians: {radians} RGB: {red} {green} {blue}");
 
+  fn convert(val: f64) -> f64 {
+    let rgb_percent = val.clamp(0.0, 2.0) / 2.0;
+    let rgb_float = rgb_percent * 255.0;
+    rgb_float.floor()
+  }
+
   // now we turn our floats into bytes
   // we have to clamp because the YUV color space is bizzare.
-  let redb: u8 = (red.clamp(0.0,1.0) * 255.0).floor() as u8;
-  let greenb: u8 = (green.clamp(0.0, 1.0) * 255.0).floor() as u8;
-  let blueb: u8 = (blue.clamp(0.0, 1.0) * 255.0).floor() as u8;
+  let redb: u8 = convert(red) as u8;
+  let greenb: u8 = convert(green) as u8;
+  let blueb: u8 = convert(blue) as u8;
 
   // then we format
-  format!("#{:02x}{:02x}{:02x}", redb, greenb, blueb)
+  let out = format!("#{:02x}{:02x}{:02x}", redb, greenb, blueb);
+
+  /*
+  if log {
+    debug!("color {color} {radians} ({red} : {}, {green} : {}, {blue} : {}) {out}",
+           convert(red), convert(green), convert(blue));
+  }
+  */
+
+  out
 }
 
 /// Search through a color map, trav
 pub fn room_cut(terrain: &LocalRoomTerrain, visual: RoomVisual, render: Render) -> Vec<RoomXY> {
-  let dist_matrix = DistMatrix::new_taxicab(terrain);
+  let height_map = DistMatrix::new_taxicab(terrain);
   // let maximas: Vec<Maxima> = find_maxima(&dist_matrix).collect();
-  let (maxima_set, mut color_map, color_count) = create_color_map(&dist_matrix);
-  // let borders = flood_color_map(&dist_matrix, &mut color_map, &maximas);
-  // let maxima_set: HashSet<RoomXY> = maximas.iter().map(|m| m.xy).collect();
+  let (maxima_iter, mut maxima_dts) = calculate_local_maxima(&height_map);
+  let maxima_set: HashSet<RoomXY> = maxima_iter.collect();
+  let maxima_colors = color_disjoint_tile_set(&height_map, &mut maxima_dts);
+  let (color_map, color_count, borders) = flood_color_map(
+    &height_map, maxima_set.iter().copied());
 
-  /*
-  for maxima in maximas.iter() {
-    debug!("{}: color = {}", maxima.xy, color_map[maxima.xy]);
-  }
-  */
+  let (disp_color_map, disp_color_count) = (color_map, color_count); // maxima_colors;
 
   for xy in all_room_xy() {
     let (x,y): (u8,u8) = xy.into();
     let fx: f32 = x.into();
     let fy: f32 = y.into();
     let num = match &render {
-      Render::Height => dist_matrix.get(xy) as u16,
-      Render::Colors => color_map[xy],
+      Render::Height => height_map.get(xy) as u16,
+      Render::Colors => disp_color_map[xy],
     };
     let num_str = format!("{:?}", num);
     let style = if maxima_set.contains(&xy) {
       TextStyle::default()
         .color("#FFFFFF")
     } else {
-      let color_str = calculate_color(color_count, color_map[xy]);
+      let color_str = calculate_color(disp_color_count, disp_color_map[xy]);
       TextStyle::default()
         .color(&color_str)
     };
